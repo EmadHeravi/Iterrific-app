7614083fc74671672d36a9090d09664e85df4633
Emad Heravi <emd.her@gmail.com>
2026-05-14 18:05:37 +0200

    drop down

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -31,8 +31,16 @@
                 >
             </a>
 
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#publicNavbar">
-                <span class="navbar-toggler-icon"></span>
+            <button
+                class="navbar-toggler border-0 shadow-none"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#publicNavbar"
+                aria-controls="publicNavbar"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+            >
+                <i class="fas fa-bars text-dark fs-4"></i>
             </button>
 
             <div class="collapse navbar-collapse" id="publicNavbar">
